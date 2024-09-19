73e8b6c4dcda5bf7f688fd4466946f2f95695d85
Jez-claimit <jez@claimit.ai>
2024-09-19 18:46:26 +0100

    Init commit
     Author:    Jez-Horton <jez_programming@outlook.com>
     Date:      Thu Sep 19 18:46:26 2024 +0100

---
+++ server/src/app.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from 'express';
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
